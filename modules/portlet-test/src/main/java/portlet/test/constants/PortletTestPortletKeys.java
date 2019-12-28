10d613035f1cab341326e4e0cffaeb079ac3f179
Ignacio Roncero Bazarra <ironcero@gmail.com>
2019-12-28 09:23:58 +0100

    first commit

---
+++ modules/portlet-test/src/main/java/portlet/test/constants/PortletTestPortletKeys.java
@@ -0,0 +1,11 @@
+package portlet.test.constants;
+
+/**
+ * @author ironcero
+ */
+public class PortletTestPortletKeys {
+
+	public static final String PORTLETTEST =
+		"portlet_test_PortletTestPortlet";
+
+}
---
+++ modules/portlet-test/src/main/java/portlet/test/portlet/PortletTestPortlet.java
@@ -0,0 +1,30 @@
+package portlet.test.portlet;
+
+import portlet.test.constants.PortletTestPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author ironcero
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=PortletTest",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + PortletTestPortletKeys.PORTLETTEST,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class PortletTestPortlet extends MVCPortlet {
+}
